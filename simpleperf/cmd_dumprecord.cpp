jbeich/platform_system_extras

simpleperf: support showing symbols for interpreted java code.
To convert from a dex_pc (returned by libunwindstack) to a symbol name,
we need below things:
1. The mapping info of the vdex file containing the dex_pc.
2. The offsets of dex files in the vdex file.

So make below changes:
1. Record none executable maps when profiling java code.
2. Refactor dso code to add a new type for dex file, using DexFileDso
   to store dex file offsets in a vdex file, and load symbols from a
   vdex files.
3. Add read_dex_file.cpp to read java symbols using libdexfile.
4. Change the format of file section in record_file_format.h, to store
   dex file offsets in vdex files.

Bug: http://b/73126888
Test: Run simpleperf to profile several apps manually, can see
Test: callstacks of both java code and native code.
Test: Run simpleperf_unit_test.

Change-Id: I08005a03beb3df1a70db034bc463f555934856ba

simpleperf/cmd_dumprecord.cpp
@@ -235,11 +235,12 @@ bool DumpRecordCommand::DumpFeatureSection() {
       uint32_t file_type;
       uint64_t min_vaddr;
       std::vector<Symbol> symbols;
+      std::vector<uint64_t> dex_file_offsets;
       size_t read_pos = 0;
       PrintIndented(1, "file:\n");
       while (record_file_reader_->ReadFileFeature(read_pos, &file_path,
                                                   &file_type, &min_vaddr,
-                                                  &symbols)) {
+                                                  &symbols, &dex_file_offsets)) {
         PrintIndented(2, "file_path %s\n", file_path.c_str());
         PrintIndented(2, "file_type %s\n", DsoTypeToString(static_cast<DsoType>(file_type)));
         PrintIndented(2, "min_vaddr 0x%" PRIx64 "\n", min_vaddr);
@@ -247,6 +248,12 @@ bool DumpRecordCommand::DumpFeatureSection() {
         for (const auto& symbol : symbols) {
           PrintIndented(3, "%s [0x%" PRIx64 "-0x%" PRIx64 "]\n", symbol.DemangledName(),
                         symbol.addr, symbol.addr + symbol.len);
+        }
+        if (file_type == static_cast<uint32_t>(DSO_DEX_FILE)) {
+          PrintIndented(2, "dex_file_offsetes:\n");
+          for (uint64_t offset : dex_file_offsets) {
+            PrintIndented(3, "0x%" PRIx64 "\n", offset);
+          }
         }
       }
     } else if (feature == FEAT_META_INFO) {

simpleperf/event_selection_set.cpp
@@ -410,6 +410,18 @@ bool EventSelectionSet::NeedKernelSymbol() const {
   return false;
 }
 
+void EventSelectionSet::RecordNoneExecutableMmap(bool record) {
+  for (auto& group : groups_) {
+    for (auto& selection : group) {
+      selection.event_attr.mmap_data = record ? 1 : 0;
+    }
+  }
+}
+
+bool EventSelectionSet::RecordNoneExecutableMmap() const {
+  return groups_[0][0].event_attr.mmap_data == 1;
+}
+
 static bool CheckIfCpusOnline(const std::vector<int>& cpus) {
   std::vector<int> online_cpus = GetOnlineCpus();
   for (const auto& cpu : cpus) {

AERA-Recovery/android_system_extras

   to store dex file offsets in a vdex file, and load symbols from that
   vdex file.
Bug: http://b/77236599

simpleperf/cmd_dumprecord.cpp
@@ -235,11 +235,12 @@ bool DumpRecordCommand::DumpFeatureSection() {
       uint32_t file_type;
       uint64_t min_vaddr;
       std::vector<Symbol> symbols;
+      std::vector<uint64_t> dex_file_offsets;
       size_t read_pos = 0;
       PrintIndented(1, "file:\n");
       while (record_file_reader_->ReadFileFeature(read_pos, &file_path,
                                                   &file_type, &min_vaddr,
-                                                  &symbols)) {
+                                                  &symbols, &dex_file_offsets)) {
         PrintIndented(2, "file_path %s\n", file_path.c_str());
         PrintIndented(2, "file_type %s\n", DsoTypeToString(static_cast<DsoType>(file_type)));
         PrintIndented(2, "min_vaddr 0x%" PRIx64 "\n", min_vaddr);
@@ -247,6 +248,12 @@ bool DumpRecordCommand::DumpFeatureSection() {
         for (const auto& symbol : symbols) {
           PrintIndented(3, "%s [0x%" PRIx64 "-0x%" PRIx64 "]\n", symbol.DemangledName(),
                         symbol.addr, symbol.addr + symbol.len);
+        }
+        if (file_type == static_cast<uint32_t>(DSO_DEX_FILE)) {
+          PrintIndented(2, "dex_file_offsets:\n");
+          for (uint64_t offset : dex_file_offsets) {
+            PrintIndented(3, "0x%" PRIx64 "\n", offset);
+          }
         }
       }
     } else if (feature == FEAT_META_INFO) {

simpleperf/event_selection_set.cpp
@@ -410,6 +410,18 @@ bool EventSelectionSet::NeedKernelSymbol() const {
   return false;
 }
 
+void EventSelectionSet::SetRecordNotExecutableMaps(bool record) {
+  for (auto& group : groups_) {
+    for (auto& selection : group) {
+      selection.event_attr.mmap_data = record ? 1 : 0;
+    }
+  }
+}
+
+bool EventSelectionSet::RecordNotExecutableMaps() const {
+  return groups_[0][0].event_attr.mmap_data == 1;
+}
+
 static bool CheckIfCpusOnline(const std::vector<int>& cpus) {
   std::vector<int> online_cpus = GetOnlineCpus();
   for (const auto& cpu : cpus) {
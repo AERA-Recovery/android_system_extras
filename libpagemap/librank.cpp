GrapheneOS/platform_system_extras

commit 9eed624c760b9c70de9a8f75ff84c5f085d62fdc

Don't invent our own pagemap constants.
Use the uapi ones instead, so I don't have to think about whether ours
are correct.

Bug: http://b/31044648
Change-Id: Ibe4a078ad3223419301a5f585e8ed24dd7366ce2

libpagemap/pm_map.c
@@ -109,7 +109,7 @@ int pm_map_workingset(pm_map_t *map, pm_memusage_t *ws_out) {
                                 &flags);
         if (error) goto out;
 
-        if (!(flags & PM_PAGE_REFERENCED))
+        if (!(flags & KPF_REFERENCED))
             continue;
 
         error = pm_kernel_count(map->proc->ker, PM_PAGEMAP_PFN(pagemap[i]),

procmem/procmem.c
@@ -206,14 +206,14 @@ int main(int argc, char *argv[]) {
                     fprintf(stderr, "error getting flags for frame.\n");
                 }
 
-                if ((ws != WS_ONLY) || (flags & PM_PAGE_REFERENCED)) {
+                if ((ws != WS_ONLY) || (flags & KPF_REFERENCED)) {
                     if (count > 1) {
-                        if (flags & PM_PAGE_DIRTY)
+                        if (flags & KPF_DIRTY)
                             mi->shared_dirty++;
                         else
                             mi->shared_clean++;
                     } else {
-                        if (flags & PM_PAGE_DIRTY)
+                        if (flags & KPF_DIRTY)
                             mi->private_dirty++;
                         else
                             mi->private_clean++;

procrank/procrank.cpp
@@ -197,9 +197,9 @@ int main(int argc, char *argv[]) {
         if (!strcmp(argv[arg], "-u")) { compfn = &sort_by_uss; continue; }
         if (!strcmp(argv[arg], "-s")) { compfn = &sort_by_swap; continue; }
         if (!strcmp(argv[arg], "-o")) { compfn = &sort_by_oomadj; oomadj = true; continue; }
-        if (!strcmp(argv[arg], "-c")) { required_flags = 0; flags_mask = PM_PAGE_SWAPBACKED; continue; }
-        if (!strcmp(argv[arg], "-C")) { required_flags = flags_mask = PM_PAGE_SWAPBACKED; continue; }
-        if (!strcmp(argv[arg], "-k")) { required_flags = flags_mask = PM_PAGE_KSM; continue; }
+        if (!strcmp(argv[arg], "-c")) { required_flags = 0; flags_mask = KPF_SWAPBACKED; continue; }
+        if (!strcmp(argv[arg], "-C")) { required_flags = flags_mask = KPF_SWAPBACKED; continue; }
+        if (!strcmp(argv[arg], "-k")) { required_flags = flags_mask = KPF_KSM; continue; }
         if (!strcmp(argv[arg], "-w")) { ws = WS_ONLY; continue; }
         if (!strcmp(argv[arg], "-W")) { ws = WS_RESET; continue; }
         if (!strcmp(argv[arg], "-R")) { order *= -1; continue; }

ksmutils/ksminfo.c
@@ -241,7 +241,7 @@ static int read_pages(struct ksm_pages *kp, pm_map_t **maps, size_t num_maps, ui
                         pagemap[i]);
                 continue;
             }
-            if (!(flags & PM_PAGE_KSM)) {
+            if (!(flags & KPF_KSM)) {
                 continue;
             }
             vaddr = pm_map_start(maps[i]) + j * pm_kernel_pagesize(ker);
@@ -482,4 +482,3 @@ exit:
 
     return rc;
 }
-

libpagemap/include/pagemap/pagemap.h
@@ -23,6 +23,8 @@
 #include <sys/types.h>
 #include <sys/queue.h>
 
+#include <linux/kernel-page-flags.h>
+
 __BEGIN_DECLS
 
 typedef struct pm_proportional_swap pm_proportional_swap_t;
@@ -124,38 +126,10 @@ int pm_kernel_pids(pm_kernel_t *ker, pid_t **pids_out, size_t *len);
 int pm_kernel_count(pm_kernel_t *ker, uint64_t pfn, uint64_t *count_out);
 
 /* Get the page flags (from /proc/kpageflags) of a physical frame.
- * The count is returned through *flags_out. */
+ * Flag constants are in <linux/kernel-page-flags.h>.
+ * The count is returned through *flags_out.
+ */
 int pm_kernel_flags(pm_kernel_t *ker, uint64_t pfn, uint64_t *flags_out);
-
-#define PM_PAGE_LOCKED     (1 <<  0)
-#define PM_PAGE_ERROR      (1 <<  1)
-#define PM_PAGE_REFERENCED (1 <<  2)
-#define PM_PAGE_UPTODATE   (1 <<  3)
-#define PM_PAGE_DIRTY      (1 <<  4)
-#define PM_PAGE_LRU        (1 <<  5)
-#define PM_PAGE_ACTIVE     (1 <<  6)
-#define PM_PAGE_SLAB       (1 <<  7)
-#define PM_PAGE_WRITEBACK  (1 <<  8)
-#define PM_PAGE_RECLAIM    (1 <<  9)
-#define PM_PAGE_BUDDY      (1 << 10)
-
-/* for kernels >= 2.6.31 */
-#define PM_PAGE_MMAP          (1 << 11)
-#define PM_PAGE_ANON          (1 << 12)
-#define PM_PAGE_SWAPCACHE     (1 << 13)
-#define PM_PAGE_SWAPBACKED    (1 << 14)
-#define PM_PAGE_COMPOUND_HEAD (1 << 15)
-#define PM_PAGE_COMPOUND_TAIL (1 << 16)
-#define PM_PAGE_HUGE          (1 << 17)
-#define PM_PAGE_UNEVICTABLE   (1 << 18)
-#define PM_PAGE_HWPOISON      (1 << 19)
-#define PM_PAGE_NOPAGE        (1 << 20)
-
-/* for kernels >= 2.6.32 */
-#define PM_PAGE_KSM           (1 << 21)
-
-/* for kernels >= 3.4 */
-#define PM_PAGE_THP           (1 << 22)
 
 /* Destroy a pm_kernel_t. */
 int pm_kernel_destroy(pm_kernel_t *ker);
@@ -180,7 +154,7 @@ int pm_process_usage_flags(pm_process_t *proc, pm_memusage_t *usage_out,
 int pm_process_workingset(pm_process_t *proc, pm_memusage_t *ws_out, int reset);
 
 /* Get the PFNs corresponding to a range of virtual addresses.
- * The array of PFNs is returned through *range_out, and the caller has the 
+ * The array of PFNs is returned through *range_out, and the caller has the
  * responsibility to free it. */
 int pm_process_pagemap_range(pm_process_t *proc,
                              uint64_t low, uint64_t hi,
@@ -197,7 +171,7 @@ int pm_process_pagemap_range(pm_process_t *proc,
 
 /* Get the maps in the virtual address space of this process.
  * Returns an array of pointers to pm_map_t through *maps.
- * The array should be freed by the caller, but the maps should not be 
+ * The array should be freed by the caller, but the maps should not be
  * modified or destroyed. */
 int pm_process_maps(pm_process_t *proc, pm_map_t ***maps_out, size_t *len);
 

librank/librank.c
@@ -264,15 +264,15 @@ int main(int argc, char *argv[]) {
             break;
         case 'c':
             required_flags = 0;
-            flags_mask = PM_PAGE_SWAPBACKED;
+            flags_mask = KPF_SWAPBACKED;
             break;
         case 'C':
-            required_flags = PM_PAGE_SWAPBACKED;
-            flags_mask = PM_PAGE_SWAPBACKED;
+            required_flags = KPF_SWAPBACKED;
+            flags_mask = KPF_SWAPBACKED;
             break;
         case 'k':
-            required_flags = PM_PAGE_KSM;
-            flags_mask = PM_PAGE_KSM;
+            required_flags = KPF_KSM;
+            flags_mask = KPF_KSM;
             break;
         case 'h':
             usage(argv[0]);

AERA-Recovery/android_system_extras

commit 3f1fbe38c7b04eb89bdd747107e11507c1720490

KPF_ constants are bit numbers, not masks.
Adam Romanek mailed me to point out that this has been broken since
9eed624c760b9c70de9a8f75ff84c5f085d62fdc in 2016.

Bug: N/A
Test: `procrank -C` on hikey
Change-Id: Icd4929396dc106175b847e876f02b449085b4846

libpagemap/librank.cpp
@@ -256,15 +256,13 @@ int main(int argc, char *argv[]) {
             break;
         case 'c':
             required_flags = 0;
-            flags_mask = KPF_SWAPBACKED;
+            flags_mask = (1 << KPF_SWAPBACKED);
             break;
         case 'C':
-            required_flags = KPF_SWAPBACKED;
-            flags_mask = KPF_SWAPBACKED;
+            required_flags = flags_mask = (1 << KPF_SWAPBACKED);
             break;
         case 'k':
-            required_flags = KPF_KSM;
-            flags_mask = KPF_KSM;
+            required_flags = flags_mask = (1 << KPF_KSM);
             break;
         case 'h':
             usage(argv[0]);

libpagemap/pm_map.c
@@ -109,8 +109,7 @@ int pm_map_workingset(pm_map_t *map, pm_memusage_t *ws_out) {
                                 &flags);
         if (error) goto out;
 
-        if (!(flags & KPF_REFERENCED))
-            continue;
+        if (!(flags & (1 << KPF_REFERENCED))) continue;
 
         error = pm_kernel_count(map->proc->ker, PM_PAGEMAP_PFN(pagemap[i]),
                                 &count);

libpagemap/procmem.c
@@ -206,17 +206,19 @@ int main(int argc, char *argv[]) {
                     fprintf(stderr, "error getting flags for frame.\n");
                 }
 
-                if ((ws != WS_ONLY) || (flags & KPF_REFERENCED)) {
+                if ((ws != WS_ONLY) || (flags & (1 << KPF_REFERENCED))) {
                     if (count > 1) {
-                        if (flags & KPF_DIRTY)
+                        if (flags & (1 << KPF_DIRTY)) {
                             mi->shared_dirty++;
-                        else
+                        } else {
                             mi->shared_clean++;
+                        }
                     } else {
-                        if (flags & KPF_DIRTY)
+                        if (flags & (1 << KPF_DIRTY)) {
                             mi->private_dirty++;
-                        else
+                        } else {
                             mi->private_clean++;
+                        }
                     }
                 }
             }

libpagemap/procrank.cpp
@@ -222,9 +222,9 @@ int main(int argc, char *argv[]) {
         if (!strcmp(argv[arg], "-u")) { compfn = &sort_by_uss; continue; }
         if (!strcmp(argv[arg], "-s")) { compfn = &sort_by_swap; continue; }
         if (!strcmp(argv[arg], "-o")) { compfn = &sort_by_oomadj; oomadj = true; continue; }
-        if (!strcmp(argv[arg], "-c")) { required_flags = 0; flags_mask = KPF_SWAPBACKED; continue; }
-        if (!strcmp(argv[arg], "-C")) { required_flags = flags_mask = KPF_SWAPBACKED; continue; }
-        if (!strcmp(argv[arg], "-k")) { required_flags = flags_mask = KPF_KSM; continue; }
+        if (!strcmp(argv[arg], "-c")) { required_flags = 0; flags_mask = (1 << KPF_SWAPBACKED); continue; }
+        if (!strcmp(argv[arg], "-C")) { required_flags = flags_mask = (1 << KPF_SWAPBACKED); continue; }
+        if (!strcmp(argv[arg], "-k")) { required_flags = flags_mask = (1 << KPF_KSM); continue; }
         if (!strcmp(argv[arg], "-w")) { ws = WS_ONLY; continue; }
         if (!strcmp(argv[arg], "-W")) { ws = WS_RESET; continue; }
         if (!strcmp(argv[arg], "-R")) { order *= -1; continue; }
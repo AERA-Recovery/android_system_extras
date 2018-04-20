jbeich/platform_system_extras

simpleperf: add symbol defore demangle in protobuf interface.
Bug: none
Test: run simpleperf_unit_test.

Change-Id: I55ce099bea257c25b2617de98ce714510e5abb9e

simpleperf/cmd_report_sample.cpp
@@ -365,6 +365,9 @@ bool ReportSampleCommand::DumpProtobufReport(const std::string& filename) {
       for (int i = 0; i < file.symbol_size(); ++i) {
         FprintIndented(report_fp_, 1, "symbol: %s\n", file.symbol(i).c_str());
       }
+      for (int i = 0; i < file.raw_symbol_size(); ++i) {
+        FprintIndented(report_fp_, 1, "raw_symbol: %s\n", file.raw_symbol(i).c_str());
+      }
       if (file.id() != files.size()) {
         LOG(ERROR) << "file id doesn't increase orderly, expected "
                    << files.size() << ", really " << file.id();
@@ -627,6 +630,8 @@ bool ReportSampleCommand::PrintFileInfoInProtobuf() {
     for (const auto& sym : dump_symbols) {
       std::string* symbol = file->add_symbol();
       *symbol = sym->DemangledName();
+      std::string* raw_symbol = file->add_raw_symbol();
+      *raw_symbol = sym->Name();
     }
     if (!WriteRecordInProtobuf(proto_record)) {
       return false;

simpleperf/cmd_report_sample_test.cpp
@@ -143,3 +143,12 @@ TEST(cmd_report_sample, show_art_frames_option) {
                     {"--show-callchain", "--show-art-frames"});
   ASSERT_NE(data.find("artMterpAsmInstructionStart"), std::string::npos);
 }
+
+TEST(cmd_report_sample, show_symbols_before_and_after_demangle) {
+  std::string data;
+  GetProtobufReport(PERF_DATA_WITH_INTERPRETER_FRAMES, &data, {"--show-callchain"});
+  ASSERT_NE(data.find("symbol: android::hardware::IPCThreadState::talkWithDriver(bool)"),
+            std::string::npos);
+  ASSERT_NE(data.find("raw_symbol: _ZN7android8hardware14IPCThreadState14talkWithDriverEb"),
+            std::string::npos);
+}

AERA-Recovery/android_system_extras

simpleperf/cmd_report_sample.cpp
@@ -365,6 +365,9 @@ bool ReportSampleCommand::DumpProtobufReport(const std::string& filename) {
       for (int i = 0; i < file.symbol_size(); ++i) {
         FprintIndented(report_fp_, 1, "symbol: %s\n", file.symbol(i).c_str());
       }
+      for (int i = 0; i < file.mangled_symbol_size(); ++i) {
+        FprintIndented(report_fp_, 1, "mangled_symbol: %s\n", file.mangled_symbol(i).c_str());
+      }
       if (file.id() != files.size()) {
         LOG(ERROR) << "file id doesn't increase orderly, expected "
                    << files.size() << ", really " << file.id();
@@ -627,6 +630,8 @@ bool ReportSampleCommand::PrintFileInfoInProtobuf() {
     for (const auto& sym : dump_symbols) {
       std::string* symbol = file->add_symbol();
       *symbol = sym->DemangledName();
+      std::string* mangled_symbol = file->add_mangled_symbol();
+      *mangled_symbol = sym->Name();
     }
     if (!WriteRecordInProtobuf(proto_record)) {
       return false;

simpleperf/cmd_report_sample_test.cpp
@@ -143,3 +143,12 @@ TEST(cmd_report_sample, show_art_frames_option) {
                     {"--show-callchain", "--show-art-frames"});
   ASSERT_NE(data.find("artMterpAsmInstructionStart"), std::string::npos);
 }
+
+TEST(cmd_report_sample, show_symbols_before_and_after_demangle) {
+  std::string data;
+  GetProtobufReport(PERF_DATA_WITH_INTERPRETER_FRAMES, &data, {"--show-callchain"});
+  ASSERT_NE(data.find("symbol: android::hardware::IPCThreadState::talkWithDriver(bool)"),
+            std::string::npos);
+  ASSERT_NE(data.find("mangled_symbol: _ZN7android8hardware14IPCThreadState14talkWithDriverEb"),
+            std::string::npos);
+}
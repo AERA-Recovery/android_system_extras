jbeich/platform_system_extras

simpleperf: Add support for implementation-defined raw PMU events.
The patch was sent to upstream by c.orakwue@samsung.com, then
I made some modifications to fit existing code.

Bug: none
Test: run simpleperf_unit_test.
Change-Id: Iccc74892aff972e2c11941c3587314cdf5f63abd

simpleperf/cmd_record.cpp
@@ -108,10 +108,12 @@ class RecordCommand : public Command {
 "\n"
 "Select monitored event types:\n"
 "-e event1[:modifier1],event2[:modifier2],...\n"
-"             Select the event list to sample. Use `simpleperf list` to find\n"
-"             all possible event names. Modifiers can be added to define how\n"
-"             the event should be monitored.\n"
-"             Possible modifiers are:\n"
+"             Select a list of events to record. An event can be:\n"
+"               1) an event name listed in `simpleperf list`;\n"
+"               2) A raw PMU event in rNNN format. NNN is a hex number.\n"
+"                  For example, r11 selected event number 0x11.\n"
+"             Modifiers can be added to define how the event should be\n"
+"             monitored. Possible modifiers are:\n"
 "                u - monitor user space events only\n"
 "                k - monitor kernel space events only\n"
 "--group event1[:modifier],event2[:modifier2],...\n"

simpleperf/cmd_stat.cpp
@@ -317,9 +317,12 @@ class StatCommand : public Command {
 "                        --interval-only-values.\n"
 "--interval-only-values  Print numbers of events happened in each interval.\n"
 "-e event1[:modifier1],event2[:modifier2],...\n"
-"                 Select the event list to count. Use `simpleperf list` to find\n"
-"                 all possible event names. Modifiers can be added to define\n"
-"                 how the event should be monitored. Possible modifiers are:\n"
+"                 Select a list of events to count. An event can be:\n"
+"                   1) an event name listed in `simpleperf list`;\n"
+"                   2) A raw PMU event in rNNN format. NNN is a hex number.\n"
+"                      For example, r11 selected event number 0x11.\n"
+"                 Modifiers can be added to define how the event should be\n"
+"                 monitored. Possible modifiers are:\n"
 "                   u - monitor user space events only\n"
 "                   k - monitor kernel space events only\n"
 "--group event1[:modifier],event2[:modifier2],...\n"

simpleperf/event_type.cpp
@@ -50,7 +50,8 @@ bool SetTracepointEventsFilePath(const std::string& filepath) {
 
 std::string GetTracepointEvents() {
   std::string result;
-  for (const EventType& event : GetAllEventTypes()) {
+  for (auto& pair : GetAllEventTypes()) {
+    const EventType& event = pair.second;
     if (event.type != PERF_TYPE_TRACEPOINT) {
       continue;
     }
@@ -110,7 +111,7 @@ static std::vector<EventType> GetTracepointEventTypes() {
   return result;
 }
 
-static std::vector<EventType> event_type_array;
+static std::map<std::string, EventType> event_type_map;
 
 std::string ScopedEventTypes::BuildString(const std::vector<const EventType*>& event_types) {
   std::string result;
@@ -124,47 +125,62 @@ std::string ScopedEventTypes::BuildString(const std::vector<const EventType*>& e
   return result;
 }
 
+static std::string ToLower(const std::string& s) {
+  std::string result(s);
+  for (auto& c : result) {
+    c = tolower(c);
+  }
+  return result;
+}
+
 ScopedEventTypes::ScopedEventTypes(const std::string& event_type_str) {
-  saved_event_types_ = std::move(event_type_array);
-  event_type_array.clear();
+  saved_event_types_ = std::move(event_type_map);
+  event_type_map.clear();
   for (auto& s : android::base::Split(event_type_str, "\n")) {
     std::string name = s.substr(0, s.find(','));
     uint32_t type;
     uint64_t config;
     sscanf(s.c_str() + name.size(), ",%u,%" PRIu64, &type, &config);
-    event_type_array.emplace_back(name, type, config, "", "");
+    event_type_map[ToLower(name)] = EventType(name, type, config, "", "");
   }
 }
 
 ScopedEventTypes::~ScopedEventTypes() {
-  event_type_array = std::move(saved_event_types_);
+  event_type_map = std::move(saved_event_types_);
 }
 
-const std::vector<EventType>& GetAllEventTypes() {
-  if (event_type_array.empty()) {
-    event_type_array.insert(event_type_array.end(), static_event_type_array.begin(),
-                            static_event_type_array.end());
+const std::map<std::string, EventType>& GetAllEventTypes() {
+  if (event_type_map.empty()) {
+    for (auto& type : static_event_type_array) {
+      event_type_map[ToLower(type.name)] = type;
+    }
     std::vector<EventType> tracepoint_array = GetTracepointEventTypes();
-    event_type_array.insert(event_type_array.end(), tracepoint_array.begin(),
-                            tracepoint_array.end());
+    for (auto& type : tracepoint_array) {
+      event_type_map[ToLower(type.name)] = type;
+    }
   }
-  return event_type_array;
+  return event_type_map;
 }
 
 const EventType* FindEventTypeByName(const std::string& name, bool report_error) {
-  const EventType* result = nullptr;
-  for (auto& event_type : GetAllEventTypes()) {
-    if (android::base::EqualsIgnoreCase(event_type.name, name)) {
-      result = &event_type;
-      break;
+  const auto& event_types = GetAllEventTypes();
+  auto it = event_types.find(ToLower(name));
+  if (it != event_types.end()) {
+    return &it->second;
+  }
+  if (!name.empty() && name[0] == 'r') {
+    char* end;
+    uint64_t config = strtoull(&name[1], &end, 16);
+    if (end != &name[1] && *end == '\0') {
+      event_type_map[name] = EventType(name, PERF_TYPE_RAW, config, "", "");
+      return &event_type_map[name];
     }
   }
-  if (result == nullptr && report_error) {
+  if (report_error) {
     LOG(ERROR) << "Unknown event_type '" << name
                << "', try `simpleperf list` to list all possible event type names";
-    return nullptr;
   }
-  return result;
+  return nullptr;
 }
 
 std::unique_ptr<EventTypeAndModifier> ParseEventType(const std::string& event_type_str) {

AERA-Recovery/android_system_extras

simpleperf/cmd_record.cpp
@@ -108,10 +108,12 @@ class RecordCommand : public Command {
 "\n"
 "Select monitored event types:\n"
 "-e event1[:modifier1],event2[:modifier2],...\n"
-"             Select the event list to sample. Use `simpleperf list` to find\n"
-"             all possible event names. Modifiers can be added to define how\n"
-"             the event should be monitored.\n"
-"             Possible modifiers are:\n"
+"             Select a list of events to record. An event can be:\n"
+"               1) an event name listed in `simpleperf list`;\n"
+"               2) a raw PMU event in rN format. N is a hex number.\n"
+"                  For example, r1b selects event number 0x1b.\n"
+"             Modifiers can be added to define how the event should be\n"
+"             monitored. Possible modifiers are:\n"
 "                u - monitor user space events only\n"
 "                k - monitor kernel space events only\n"
 "--group event1[:modifier],event2[:modifier2],...\n"

simpleperf/cmd_stat.cpp
@@ -317,9 +317,12 @@ class StatCommand : public Command {
 "                        --interval-only-values.\n"
 "--interval-only-values  Print numbers of events happened in each interval.\n"
 "-e event1[:modifier1],event2[:modifier2],...\n"
-"                 Select the event list to count. Use `simpleperf list` to find\n"
-"                 all possible event names. Modifiers can be added to define\n"
-"                 how the event should be monitored. Possible modifiers are:\n"
+"                 Select a list of events to count. An event can be:\n"
+"                   1) an event name listed in `simpleperf list`;\n"
+"                   2) a raw PMU event in rN format. N is a hex number.\n"
+"                      For example, r1b selects event number 0x1b.\n"
+"                 Modifiers can be added to define how the event should be\n"
+"                 monitored. Possible modifiers are:\n"
 "                   u - monitor user space events only\n"
 "                   k - monitor kernel space events only\n"
 "--group event1[:modifier],event2[:modifier2],...\n"

simpleperf/event_type.cpp
@@ -50,7 +50,7 @@ bool SetTracepointEventsFilePath(const std::string& filepath) {
 
 std::string GetTracepointEvents() {
   std::string result;
-  for (const EventType& event : GetAllEventTypes()) {
+  for (auto& event : GetAllEventTypes()) {
     if (event.type != PERF_TYPE_TRACEPOINT) {
       continue;
     }
@@ -110,7 +110,7 @@ static std::vector<EventType> GetTracepointEventTypes() {
   return result;
 }
 
-static std::vector<EventType> event_type_array;
+static std::set<EventType> g_event_types;
 
 std::string ScopedEventTypes::BuildString(const std::vector<const EventType*>& event_types) {
   std::string result;
@@ -125,46 +125,50 @@ std::string ScopedEventTypes::BuildString(const std::vector<const EventType*>& e
 }
 
 ScopedEventTypes::ScopedEventTypes(const std::string& event_type_str) {
-  saved_event_types_ = std::move(event_type_array);
-  event_type_array.clear();
+  saved_event_types_ = std::move(g_event_types);
+  g_event_types.clear();
   for (auto& s : android::base::Split(event_type_str, "\n")) {
     std::string name = s.substr(0, s.find(','));
     uint32_t type;
     uint64_t config;
     sscanf(s.c_str() + name.size(), ",%u,%" PRIu64, &type, &config);
-    event_type_array.emplace_back(name, type, config, "", "");
+    g_event_types.emplace(name, type, config, "", "");
   }
 }
 
 ScopedEventTypes::~ScopedEventTypes() {
-  event_type_array = std::move(saved_event_types_);
+  g_event_types = std::move(saved_event_types_);
 }
 
-const std::vector<EventType>& GetAllEventTypes() {
-  if (event_type_array.empty()) {
-    event_type_array.insert(event_type_array.end(), static_event_type_array.begin(),
-                            static_event_type_array.end());
+const std::set<EventType>& GetAllEventTypes() {
+  if (g_event_types.empty()) {
+    g_event_types.insert(static_event_type_array.begin(), static_event_type_array.end());
     std::vector<EventType> tracepoint_array = GetTracepointEventTypes();
-    event_type_array.insert(event_type_array.end(), tracepoint_array.begin(),
-                            tracepoint_array.end());
+    g_event_types.insert(tracepoint_array.begin(), tracepoint_array.end());
   }
-  return event_type_array;
+  return g_event_types;
 }
 
 const EventType* FindEventTypeByName(const std::string& name, bool report_error) {
-  const EventType* result = nullptr;
-  for (auto& event_type : GetAllEventTypes()) {
-    if (android::base::EqualsIgnoreCase(event_type.name, name)) {
-      result = &event_type;
-      break;
+  const auto& event_types = GetAllEventTypes();
+  auto it = event_types.find(EventType(name, 0, 0, "", ""));
+  if (it != event_types.end()) {
+    return &*it;
+  }
+  if (!name.empty() && name[0] == 'r') {
+    char* end;
+    uint64_t config = strtoull(&name[1], &end, 16);
+    if (end != &name[1] && *end == '\0') {
+      auto result = g_event_types.emplace(name, PERF_TYPE_RAW, config, "", "");
+      CHECK(result.second);
+      return &*(result.first);
     }
   }
-  if (result == nullptr && report_error) {
+  if (report_error) {
     LOG(ERROR) << "Unknown event_type '" << name
                << "', try `simpleperf list` to list all possible event type names";
-    return nullptr;
   }
-  return result;
+  return nullptr;
 }
 
 std::unique_ptr<EventTypeAndModifier> ParseEventType(const std::string& event_type_str) {
jbeich/platform_system_extras

simpleperf: record complete process name.
The kernel limits length of process name to 15. But many app processes
have name with length longer than 15. This patch records complete
process name: When receiving a comm record for a process, try reading
complete name from /proc/pid/cmdline and storing the name in the comm
record.

Bug: none
Test: run simpleperf manually, it shows complete app name in reports.
Test: run simpleperf_unit_test.
Change-Id: Id29f2a2522ef5d2949828450be2d9d2f508a328d

simpleperf/cmd_record.cpp
@@ -277,7 +277,7 @@ class RecordCommand : public Command {
   bool ProcessJITDebugInfo(const std::vector<JITSymFile>& jit_symfiles,
                            const std::vector<DexSymFile>& dex_symfiles, bool sync_kernel_records);
 
-  void UpdateRecordForEmbeddedPath(Record* record);
+  void UpdateRecord(Record* record);
   bool UnwindRecord(SampleRecord& r);
   bool PostUnwindRecords();
   bool JoinCallChains();
@@ -1037,8 +1037,8 @@ bool RecordCommand::DumpProcessMaps(pid_t pid, const std::unordered_set<pid_t>& 
     }
   }
   // Dump process name.
-  std::string name;
-  if (GetThreadName(pid, &name)) {
+  std::string name = GetCompleteProcessName(pid);
+  if (!name.empty()) {
     CommRecord record(attr, pid, pid, name, event_id, last_record_timestamp_);
     if (!ProcessRecord(&record)) {
       return false;
@@ -1057,7 +1057,7 @@ bool RecordCommand::DumpProcessMaps(pid_t pid, const std::unordered_set<pid_t>& 
 }
 
 bool RecordCommand::ProcessRecord(Record* record) {
-  UpdateRecordForEmbeddedPath(record);
+  UpdateRecord(record);
   if (ShouldOmitRecord(record)) {
     return true;
   }
@@ -1241,12 +1241,20 @@ void UpdateMmapRecordForEmbeddedPath(RecordType& r, bool has_prot, uint32_t prot
   }
 }
 
-void RecordCommand::UpdateRecordForEmbeddedPath(Record* record) {
+void RecordCommand::UpdateRecord(Record* record) {
   if (record->type() == PERF_RECORD_MMAP) {
     UpdateMmapRecordForEmbeddedPath(*static_cast<MmapRecord*>(record), false, 0);
   } else if (record->type() == PERF_RECORD_MMAP2) {
     auto r = static_cast<Mmap2Record*>(record);
     UpdateMmapRecordForEmbeddedPath(*r, true, r->data->prot);
+  } else if (record->type() == PERF_RECORD_COMM) {
+    auto r = static_cast<CommRecord*>(record);
+    if (r->data->pid == r->data->tid) {
+      std::string s = GetCompleteProcessName(r->data->pid);
+      if (!s.empty()) {
+        r->SetComm(s);
+      }
+    }
   }
 }
 

simpleperf/record.h
@@ -327,6 +327,8 @@ struct CommRecord : public Record {
   CommRecord(const perf_event_attr& attr, uint32_t pid, uint32_t tid,
              const std::string& comm, uint64_t event_id, uint64_t time);
 
+  void SetComm(const std::string& name);
+
  protected:
   void DumpData(size_t indent) const override;
 };

simpleperf/record_test.cpp
@@ -131,3 +131,17 @@ TEST_F(RecordTest, SampleRecord_AdjustCallChainGeneratedByKernel) {
   expected.header.misc = PERF_RECORD_MISC_KERNEL;
   CheckRecordEqual(r, expected);
 }
+
+TEST_F(RecordTest, CommRecord) {
+  CommRecord r(event_attr, 1, 2, "init_name", 3, 4);
+  size_t record_size = r.size();
+  std::string new_name = "a_much_longer_name";
+  r.SetComm(new_name);
+  ASSERT_EQ(r.size(), record_size + 8);
+  ASSERT_EQ(std::string(r.comm), new_name);
+  ASSERT_EQ(r.data->pid, 1u);
+  ASSERT_EQ(r.data->tid, 2u);
+  ASSERT_EQ(r.sample_id.id_data.id, 3u);
+  ASSERT_EQ(r.sample_id.time_data.time, 4u);
+  CheckRecordMatchBinary(r);
+}

AERA-Recovery/android_system_extras

simpleperf/cmd_record.cpp
@@ -277,7 +277,7 @@ class RecordCommand : public Command {
   bool ProcessJITDebugInfo(const std::vector<JITSymFile>& jit_symfiles,
                            const std::vector<DexSymFile>& dex_symfiles, bool sync_kernel_records);
 
-  void UpdateRecordForEmbeddedPath(Record* record);
+  void UpdateRecord(Record* record);
   bool UnwindRecord(SampleRecord& r);
   bool PostUnwindRecords();
   bool JoinCallChains();
@@ -1037,8 +1037,8 @@ bool RecordCommand::DumpProcessMaps(pid_t pid, const std::unordered_set<pid_t>& 
     }
   }
   // Dump process name.
-  std::string name;
-  if (GetThreadName(pid, &name)) {
+  std::string name = GetCompleteProcessName(pid);
+  if (!name.empty()) {
     CommRecord record(attr, pid, pid, name, event_id, last_record_timestamp_);
     if (!ProcessRecord(&record)) {
       return false;
@@ -1057,7 +1057,7 @@ bool RecordCommand::DumpProcessMaps(pid_t pid, const std::unordered_set<pid_t>& 
 }
 
 bool RecordCommand::ProcessRecord(Record* record) {
-  UpdateRecordForEmbeddedPath(record);
+  UpdateRecord(record);
   if (ShouldOmitRecord(record)) {
     return true;
   }
@@ -1241,12 +1241,20 @@ void UpdateMmapRecordForEmbeddedPath(RecordType& r, bool has_prot, uint32_t prot
   }
 }
 
-void RecordCommand::UpdateRecordForEmbeddedPath(Record* record) {
+void RecordCommand::UpdateRecord(Record* record) {
   if (record->type() == PERF_RECORD_MMAP) {
     UpdateMmapRecordForEmbeddedPath(*static_cast<MmapRecord*>(record), false, 0);
   } else if (record->type() == PERF_RECORD_MMAP2) {
     auto r = static_cast<Mmap2Record*>(record);
     UpdateMmapRecordForEmbeddedPath(*r, true, r->data->prot);
+  } else if (record->type() == PERF_RECORD_COMM) {
+    auto r = static_cast<CommRecord*>(record);
+    if (r->data->pid == r->data->tid) {
+      std::string s = GetCompleteProcessName(r->data->pid);
+      if (!s.empty()) {
+        r->SetCommandName(s);
+      }
+    }
   }
 }
 

simpleperf/record.h
@@ -327,6 +327,8 @@ struct CommRecord : public Record {
   CommRecord(const perf_event_attr& attr, uint32_t pid, uint32_t tid,
              const std::string& comm, uint64_t event_id, uint64_t time);
 
+  void SetCommandName(const std::string& name);
+
  protected:
   void DumpData(size_t indent) const override;
 };

simpleperf/record_test.cpp
@@ -131,3 +131,17 @@ TEST_F(RecordTest, SampleRecord_AdjustCallChainGeneratedByKernel) {
   expected.header.misc = PERF_RECORD_MISC_KERNEL;
   CheckRecordEqual(r, expected);
 }
+
+TEST_F(RecordTest, CommRecord) {
+  CommRecord r(event_attr, 1, 2, "init_name", 3, 4);
+  size_t record_size = r.size();
+  std::string new_name = "a_much_longer_name";
+  r.SetCommandName(new_name);
+  ASSERT_EQ(r.size(), record_size + 8);
+  ASSERT_EQ(std::string(r.comm), new_name);
+  ASSERT_EQ(r.data->pid, 1u);
+  ASSERT_EQ(r.data->tid, 2u);
+  ASSERT_EQ(r.sample_id.id_data.id, 3u);
+  ASSERT_EQ(r.sample_id.time_data.time, 4u);
+  CheckRecordMatchBinary(r);
+}
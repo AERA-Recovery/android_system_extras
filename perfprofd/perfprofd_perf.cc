jbeich/platform_system_extras

Perfprofd: Expose supported counters from simpleperf
Add code to collect the supported counters.

Bug: 110555909
Test: mmma system/extras/perfprofd
Test: perfprofd_test
Change-Id: I51e22b0efb54ec05dff67bb5bc75d3b701fa7f56

perfprofd/perfprofd_perf.cc
@@ -19,6 +19,7 @@
 
 
 #include <assert.h>
+#include <inttypes.h>
 #include <signal.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -34,11 +35,21 @@
 #include <android-base/logging.h>
 #include <android-base/stringprintf.h>
 #include <android-base/strings.h>
+#include <android-base/unique_fd.h>
 
 #include "config.h"
 
 namespace android {
 namespace perfprofd {
+
+namespace {
+
+std::vector<std::string>& GetSupportedPerfCountersInternal() {
+  static std::vector<std::string>& vec = *new std::vector<std::string>();
+  return vec;
+}
+
+}  // namespace
 
 //
 // Invoke "perf record". Return value is OK_PROFILE_COLLECTION for
@@ -200,6 +211,99 @@ PerfResult InvokePerf(Config& config,
   }
 
   return PerfResult::kRecordFailed;
+}
+
+bool FindSupportedPerfCounters(const std::string& perf_path) {
+  const char* argv[] = { perf_path.c_str(), "list", nullptr };
+
+  base::unique_fd link[2];
+  {
+    int link_fd[2];
+
+    if (pipe(link_fd) == -1) {
+      PLOG(ERROR) << "Pipe failed";
+      return false;
+    }
+    link[0].reset(link_fd[0]);
+    link[1].reset(link_fd[1]);
+  }
+
+  pid_t pid = fork();
+
+  if (pid == -1) {
+    PLOG(ERROR) << "Fork failed";
+    return PerfResult::kForkFailed;
+  }
+
+  if (pid == 0) {
+    // Child
+
+    // Redirect stdout and stderr.
+    dup2(link[1].get(), STDOUT_FILENO);
+    dup2(link[1].get(), STDERR_FILENO);
+
+    link[0].reset();
+    link[1].reset();
+
+    // exec
+    execvp(argv[0], const_cast<char* const*>(argv));
+    PLOG(WARNING) << "exec failed";
+    exit(1);
+    __builtin_unreachable();
+  }
+
+  link[1].reset();
+
+  char buffer[128] = { 0 };
+  ssize_t bytes_read = 0;
+  std::string result;
+  while (TEMP_FAILURE_RETRY(bytes_read = read(link[0].get(), buffer, 128)) > 0) {
+    result += std::string(buffer, bytes_read);
+  }
+
+  link[0].reset();
+
+  int status_code;
+  if (waitpid(pid, &status_code, 0) == -1) {
+    LOG(WARNING) << "Failed to wait for " << perf_path << " list";
+    return false;
+  }
+
+  if (!WIFEXITED(status_code) || WEXITSTATUS(status_code) != 0) {
+    LOG(WARNING) << perf_path << " list did not exit normally.";
+    return false;
+  }
+
+  std::vector<std::string>& supported = GetSupportedPerfCountersInternal();
+  supported.clear();
+
+  // Could implement something with less memory requirements. But for now this is good
+  // enough.
+  std::vector<std::string> lines = base::Split(result, "\n");
+  for (const std::string& line : lines) {
+    if (line.length() < 2 || line.compare(0, 2, "  ") != 0) {
+      continue;
+    }
+    const size_t comment = line.find('#');
+    const size_t space = line.find(' ', 2);
+    size_t end = std::min(space, comment);
+    if (end != std::string::npos) {
+      // Scan backwards.
+      --end;
+      while (end > 2 && isspace(line[end])) {
+        end--;
+      }
+    }
+    if (end > 2) {
+      supported.push_back(line.substr(2, end - 2));
+    }
+  }
+
+  return true;
+}
+
+const std::vector<std::string>& GetSupportedPerfCounters() {
+  return GetSupportedPerfCountersInternal();
 }
 
 }  // namespace perfprofd

perfprofd/tests/perfprofd_test.cc
@@ -44,6 +44,7 @@
 #include "map_utils.h"
 #include "perfprofdcore.h"
 #include "perfprofd_cmdline.h"
+#include "perfprofd_perf.h"
 #include "perfprofd_threaded_handler.h"
 #include "quipper_helper.h"
 #include "symbolizer.h"
@@ -1131,6 +1132,24 @@ TEST_F(PerfProfdTest, CallchainRunWithCannedPerf)
 }
 
 #ifdef __ANDROID__
+
+TEST_F(PerfProfdTest, GetSupportedPerfCounters)
+{
+  // Check basic perf counters.
+  {
+    struct DummyConfig : public Config {
+      void Sleep(size_t seconds) override {}
+      bool IsProfilingEnabled() const override { return false; }
+    };
+    DummyConfig config;
+    ASSERT_TRUE(android::perfprofd::FindSupportedPerfCounters(config.perf_path));
+  }
+  const std::vector<std::string>& counters = android::perfprofd::GetSupportedPerfCounters();
+  EXPECT_TRUE(std::find(counters.begin(), counters.end(), std::string("cpu-cycles"))
+                  != counters.end()) << android::base::Join(counters, ',');
+  EXPECT_TRUE(std::find(counters.begin(), counters.end(), std::string("page-faults"))
+                  != counters.end()) << android::base::Join(counters, ',');
+}
 
 TEST_F(PerfProfdTest, BasicRunWithLivePerf)
 {

AERA-Recovery/android_system_extras

perfprofd/perfprofd_perf.cc
@@ -18,7 +18,7 @@
 #include "perfprofd_perf.h"
 
 
-#include <assert.h>
+#include <inttypes.h>
 #include <signal.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -34,11 +34,21 @@
 #include <android-base/logging.h>
 #include <android-base/stringprintf.h>
 #include <android-base/strings.h>
+#include <android-base/unique_fd.h>
 
 #include "config.h"
 
 namespace android {
 namespace perfprofd {
+
+namespace {
+
+std::unordered_set<std::string>& GetSupportedPerfCountersInternal() {
+  static std::unordered_set<std::string>& vec = *new std::unordered_set<std::string>();
+  return vec;
+}
+
+}  // namespace
 
 //
 // Invoke "perf record". Return value is OK_PROFILE_COLLECTION for
@@ -200,6 +210,97 @@ PerfResult InvokePerf(Config& config,
   }
 
   return PerfResult::kRecordFailed;
+}
+
+bool FindSupportedPerfCounters(const std::string& perf_path) {
+  const char* argv[] = { perf_path.c_str(), "list", nullptr };
+
+  base::unique_fd link[2];
+  {
+    int link_fd[2];
+
+    if (pipe(link_fd) == -1) {
+      PLOG(ERROR) << "Pipe failed";
+      return false;
+    }
+    link[0].reset(link_fd[0]);
+    link[1].reset(link_fd[1]);
+  }
+
+  pid_t pid = fork();
+
+  if (pid == -1) {
+    PLOG(ERROR) << "Fork failed";
+    return PerfResult::kForkFailed;
+  }
+
+  if (pid == 0) {
+    // Child
+
+    // Redirect stdout and stderr.
+    dup2(link[1].get(), STDOUT_FILENO);
+    dup2(link[1].get(), STDERR_FILENO);
+
+    link[0].reset();
+    link[1].reset();
+
+    // exec
+    execvp(argv[0], const_cast<char* const*>(argv));
+    PLOG(WARNING) << "exec failed";
+    exit(1);
+    __builtin_unreachable();
+  }
+
+  link[1].reset();
+
+  std::string result;
+  if (!android::base::ReadFdToString(link[0].get(), &result)) {
+    PLOG(WARNING) << perf_path << " list reading failed.";
+  }
+
+  link[0].reset();
+
+  int status_code;
+  if (waitpid(pid, &status_code, 0) == -1) {
+    LOG(WARNING) << "Failed to wait for " << perf_path << " list";
+    return false;
+  }
+
+  if (!WIFEXITED(status_code) || WEXITSTATUS(status_code) != 0) {
+    LOG(WARNING) << perf_path << " list did not exit normally.";
+    return false;
+  }
+
+  std::unordered_set<std::string>& supported = GetSupportedPerfCountersInternal();
+  supported.clear();
+
+  // Could implement something with less memory requirements. But for now this is good
+  // enough.
+  std::vector<std::string> lines = base::Split(result, "\n");
+  for (const std::string& line : lines) {
+    if (line.length() < 2 || line.compare(0, 2, "  ") != 0) {
+      continue;
+    }
+    const size_t comment = line.find('#');
+    const size_t space = line.find(' ', 2);
+    size_t end = std::min(space, comment);
+    if (end != std::string::npos) {
+      // Scan backwards.
+      --end;
+      while (end > 2 && isspace(line[end])) {
+        end--;
+      }
+    }
+    if (end > 2) {
+      supported.insert(line.substr(2, end - 2));
+    }
+  }
+
+  return true;
+}
+
+const std::unordered_set<std::string>& GetSupportedPerfCounters() {
+  return GetSupportedPerfCountersInternal();
 }
 
 }  // namespace perfprofd

perfprofd/tests/perfprofd_test.cc
@@ -22,6 +22,7 @@
 #include <mutex>
 #include <regex>
 #include <string>
+#include <unordered_set>
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -44,6 +45,7 @@
 #include "map_utils.h"
 #include "perfprofdcore.h"
 #include "perfprofd_cmdline.h"
+#include "perfprofd_perf.h"
 #include "perfprofd_threaded_handler.h"
 #include "quipper_helper.h"
 #include "symbolizer.h"
@@ -1131,6 +1133,24 @@ TEST_F(PerfProfdTest, CallchainRunWithCannedPerf)
 }
 
 #ifdef __ANDROID__
+
+TEST_F(PerfProfdTest, GetSupportedPerfCounters)
+{
+  // Check basic perf counters.
+  {
+    struct DummyConfig : public Config {
+      void Sleep(size_t seconds) override {}
+      bool IsProfilingEnabled() const override { return false; }
+    };
+    DummyConfig config;
+    ASSERT_TRUE(android::perfprofd::FindSupportedPerfCounters(config.perf_path));
+  }
+  const std::unordered_set<std::string>& counters = android::perfprofd::GetSupportedPerfCounters();
+  EXPECT_TRUE(std::find(counters.begin(), counters.end(), std::string("cpu-cycles"))
+                  != counters.end()) << android::base::Join(counters, ',');
+  EXPECT_TRUE(std::find(counters.begin(), counters.end(), std::string("page-faults"))
+                  != counters.end()) << android::base::Join(counters, ',');
+}
 
 TEST_F(PerfProfdTest, BasicRunWithLivePerf)
 {
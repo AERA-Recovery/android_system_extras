jbeich/platform_system_extras

Make lpdump possible without root.
Test: adb unroot && adb shell lpdump
Bug: 126233777

Change-Id: Ie559072fe53fee43d7c4a9e8f97a89d979e5b9bc

partition_tools/lpdump.cc
@@ -28,6 +28,7 @@
 #include <android-base/strings.h>
 #include <android-base/parseint.h>
 #ifdef __ANDROID__
+#include <cutils/android_get_control_file.h>
 #include <fs_mgr.h>
 #endif
 #include <liblp/liblp.h>
@@ -77,14 +78,37 @@ static bool IsBlockDevice(const char* file) {
     return !stat(file, &s) && S_ISBLK(s.st_mode);
 }
 
+// Reimplementation of fs_mgr_get_slot_suffix() without reading
+// kernel commandline.
+static std::string GetSlotSuffix() {
+    return base::GetProperty("ro.boot.slot_suffix", "");
+}
+
+// Reimplementation of fs_mgr_get_super_partition_name() without reading
+// kernel commandline. Always return the super partition at current slot.
+static std::string GetSuperPartionName() {
+    std::string super_partition = base::GetProperty("ro.boot.super_partition", "");
+    if (super_partition.empty()) {
+        return LP_METADATA_DEFAULT_PARTITION_NAME;
+    }
+    return super_partition + GetSlotSuffix();
+}
+
 class FileOrBlockDeviceOpener final : public PartitionOpener {
 public:
     android::base::unique_fd Open(const std::string& path, int flags) const override {
         // Try a local file first.
-        android::base::unique_fd fd(open(path.c_str(), flags));
-        if (fd >= 0) {
-            return fd;
+        auto real = std::unique_ptr<char, decltype(&free)>(realpath(path.c_str(), nullptr), &free);
+        if (real) {
+            android::base::unique_fd fd;
+#ifdef __ANDROID__
+            fd.reset(android_get_control_file(real.get()));
+            if (fd >= 0) return fd;
+#endif
+            fd.reset(open(real.get(), flags));
+            if (fd >= 0) return fd;
         }
+
         return PartitionOpener::Open(path, flags);
     }
 };
@@ -95,6 +119,9 @@ int LpdumpMain(int argc, char* argv[], std::ostream& cout, std::ostream& cerr) {
         { "help", no_argument, nullptr, 'h' },
         { nullptr, 0, nullptr, 0 },
     };
+
+    // Allow this function to be invoked by lpdumpd multiple times.
+    optind = 1;
 
     int rv;
     int index;
@@ -122,8 +149,8 @@ int LpdumpMain(int argc, char* argv[], std::ostream& cout, std::ostream& cerr) {
         }
     } else {
 #ifdef __ANDROID__
-        auto slot_number = SlotNumberForSlotSuffix(fs_mgr_get_slot_suffix());
-        pt = ReadMetadata(fs_mgr_get_super_partition_name(), slot_number);
+        auto slot_number = SlotNumberForSlotSuffix(GetSlotSuffix());
+        pt = ReadMetadata(GetSuperPartionName(), slot_number);
 #else
         return usage(argc, argv, cerr);
 #endif
@@ -190,6 +217,6 @@ int LpdumpMain(int argc, char* argv[], std::ostream& cout, std::ostream& cerr) {
     return EX_OK;
 }
 
-int main(int argc, char* argv[]) {
+int LpdumpMain(int argc, char* argv[]) {
     return LpdumpMain(argc, argv, std::cout, std::cerr);
 }

partition_tools/lpdumpd.cc
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <limits>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+#include <android/lpdump/BnLpdump.h>
+#include <android/lpdump/ILpdump.h>
+#include <binder/IPCThreadState.h>
+#include <binder/IServiceManager.h>
+#include <binder/ProcessState.h>
+
+int LpdumpMain(int argc, char* argv[], std::ostream&, std::ostream&);
+
+namespace android {
+namespace lpdump {
+
+using binder::Status;
+
+class Lpdump : public BnLpdump {
+  public:
+    Lpdump() = default;
+    virtual ~Lpdump() = default;
+
+    Status run(const std::vector<std::string>& args, std::string* aidl_return) override {
+        if (args.size() > std::numeric_limits<int>::max()) {
+            return Status::fromExceptionCode(Status::EX_ILLEGAL_ARGUMENT);
+        }
+        std::vector<std::string> m_args = args;
+        char* argv[m_args.size()];
+        for (size_t i = 0; i < m_args.size(); ++i) {
+            argv[i] = m_args[i].data();
+        }
+        LOG(INFO) << "Dumping with args: " << base::Join(args, " ");
+        std::stringstream output;
+        int ret = LpdumpMain((int)m_args.size(), argv, output, output);
+        if (ret == 0) {
+            *aidl_return = output.str();
+            return Status::ok();
+        } else {
+            return Status::fromServiceSpecificError(ret, output.str().c_str());
+        }
+    }
+};
+
+}  // namespace lpdump
+}  // namespace android
+
+int main(int, char**) {
+    using namespace android;
+
+    sp<lpdump::Lpdump> service = new lpdump::Lpdump();
+    defaultServiceManager()->addService(String16("lpdump_service"), service);
+    LOG(INFO) << "lpdumpd starting";
+    ProcessState::self()->startThreadPool();
+    IPCThreadState::self()->joinThreadPool();
+    return 0;
+}

AERA-Recovery/android_system_extras

partition_tools/lpdump.cc
@@ -28,6 +28,7 @@
 #include <android-base/strings.h>
 #include <android-base/parseint.h>
 #ifdef __ANDROID__
+#include <cutils/android_get_control_file.h>
 #include <fs_mgr.h>
 #endif
 #include <liblp/liblp.h>
@@ -77,14 +78,35 @@ static bool IsBlockDevice(const char* file) {
     return !stat(file, &s) && S_ISBLK(s.st_mode);
 }
 
+// Reimplementation of fs_mgr_get_slot_suffix() without reading
+// kernel commandline.
+static std::string GetSlotSuffix() {
+    return base::GetProperty("ro.boot.slot_suffix", "");
+}
+
+// Reimplementation of fs_mgr_get_super_partition_name() without reading
+// kernel commandline. Always return the super partition at current slot.
+static std::string GetSuperPartionName() {
+    std::string super_partition = base::GetProperty("ro.boot.super_partition", "");
+    if (super_partition.empty()) {
+        return LP_METADATA_DEFAULT_PARTITION_NAME;
+    }
+    return super_partition + GetSlotSuffix();
+}
+
 class FileOrBlockDeviceOpener final : public PartitionOpener {
 public:
     android::base::unique_fd Open(const std::string& path, int flags) const override {
         // Try a local file first.
-        android::base::unique_fd fd(open(path.c_str(), flags));
-        if (fd >= 0) {
-            return fd;
-        }
+        android::base::unique_fd fd;
+
+#ifdef __ANDROID__
+        fd.reset(android_get_control_file(path.c_str()));
+        if (fd >= 0) return fd;
+#endif
+        fd.reset(open(path.c_str(), flags));
+        if (fd >= 0) return fd;
+
         return PartitionOpener::Open(path, flags);
     }
 };
@@ -95,6 +117,9 @@ int LpdumpMain(int argc, char* argv[], std::ostream& cout, std::ostream& cerr) {
         { "help", no_argument, nullptr, 'h' },
         { nullptr, 0, nullptr, 0 },
     };
+
+    // Allow this function to be invoked by lpdumpd multiple times.
+    optind = 1;
 
     int rv;
     int index;
@@ -122,8 +147,8 @@ int LpdumpMain(int argc, char* argv[], std::ostream& cout, std::ostream& cerr) {
         }
     } else {
 #ifdef __ANDROID__
-        auto slot_number = SlotNumberForSlotSuffix(fs_mgr_get_slot_suffix());
-        pt = ReadMetadata(fs_mgr_get_super_partition_name(), slot_number);
+        auto slot_number = SlotNumberForSlotSuffix(GetSlotSuffix());
+        pt = ReadMetadata(GetSuperPartionName(), slot_number);
 #else
         return usage(argc, argv, cerr);
 #endif
@@ -190,6 +215,6 @@ int LpdumpMain(int argc, char* argv[], std::ostream& cout, std::ostream& cerr) {
     return EX_OK;
 }
 
-int main(int argc, char* argv[]) {
+int LpdumpMain(int argc, char* argv[]) {
     return LpdumpMain(argc, argv, std::cout, std::cerr);
 }

partition_tools/lpdumpd.cc
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <limits>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+#include <android/lpdump/BnLpdump.h>
+#include <android/lpdump/ILpdump.h>
+#include <binder/IPCThreadState.h>
+#include <binder/IServiceManager.h>
+#include <binder/ProcessState.h>
+
+int LpdumpMain(int argc, char* argv[], std::ostream&, std::ostream&);
+
+namespace android {
+namespace lpdump {
+
+using binder::Status;
+
+class Lpdump : public BnLpdump {
+  public:
+    Lpdump() = default;
+    virtual ~Lpdump() = default;
+
+    Status run(const std::vector<std::string>& args, std::string* aidl_return) override {
+        if (args.size() > std::numeric_limits<int>::max()) {
+            return Status::fromExceptionCode(Status::EX_ILLEGAL_ARGUMENT);
+        }
+        std::vector<std::string> m_args = args;
+        char* argv[m_args.size()];
+        for (size_t i = 0; i < m_args.size(); ++i) {
+            argv[i] = m_args[i].data();
+        }
+        LOG(DEBUG) << "Dumping with args: " << base::Join(args, " ");
+        std::stringstream output;
+        int ret = LpdumpMain((int)m_args.size(), argv, output, output);
+        if (ret == 0) {
+            *aidl_return = output.str();
+            return Status::ok();
+        } else {
+            return Status::fromServiceSpecificError(ret, output.str().c_str());
+        }
+    }
+};
+
+}  // namespace lpdump
+}  // namespace android
+
+int main(int, char**) {
+    using namespace android;
+
+    sp<lpdump::Lpdump> service = new lpdump::Lpdump();
+    defaultServiceManager()->addService(String16("lpdump_service"), service);
+    LOG(VERBOSE) << "lpdumpd starting";
+    ProcessState::self()->startThreadPool();
+    IPCThreadState::self()->joinThreadPool();
+    return 0;
+}
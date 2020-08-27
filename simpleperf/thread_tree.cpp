jbeich/platform_system_extras

simpleperf: add perf symbol map dso + test
Bug: 164091700
Test: simpleperf_unit_test
Change-Id: I73c56d87518e22146dd7ec62c52ed80640369b59

simpleperf/thread_tree.cpp
@@ -25,6 +25,7 @@
 
 #include "perf_event.h"
 #include "record.h"
+#include "utils.h"
 
 namespace simpleperf {
 
@@ -131,6 +132,34 @@ void ThreadTree::AddThreadMap(int pid, int tid, uint64_t start_addr, uint64_t le
   InsertMap(*thread->maps, MapEntry(start_addr, len, pgoff, dso, false, flags));
 }
 
+void ThreadTree::AddThreadMapsForDsoSymbols(ThreadEntry* thread, Dso* dso) {
+  const uint64_t page_size = GetPageSize();
+
+  auto maps = thread->maps;
+
+  uint64_t map_start = 0;
+  uint64_t map_end = 0;
+
+  for (const auto& sym : dso->GetSymbols()) {
+    uint64_t sym_map_start = AlignDown(sym.addr, page_size);
+    uint64_t sym_map_end = Align(sym.addr + sym.len, page_size);
+
+    if (map_end < sym_map_start) {
+      if (map_start < map_end) {
+        InsertMap(*maps, MapEntry(map_start, map_end - map_start, map_start, dso, false, 0));
+      }
+      map_start = sym_map_start;
+    }
+    if (map_end < sym_map_end) {
+      map_end = sym_map_end;
+    }
+  }
+
+  if (map_start < map_end) {
+    InsertMap(*maps, MapEntry(map_start, map_end - map_start, map_start, dso, false, 0));
+  }
+}
+
 Dso* ThreadTree::FindUserDsoOrNew(const std::string& filename, uint64_t start_addr,
                                   DsoType dso_type) {
   auto it = user_dso_tree_.find(filename);
@@ -142,6 +171,27 @@ Dso* ThreadTree::FindUserDsoOrNew(const std::string& filename, uint64_t start_ad
     it = pair.first;
   }
   return it->second.get();
+}
+
+namespace {
+
+// Real map file path depends on where the process can create files.
+// For example, app can create files only in its data directory.
+// Use normalized name inherited from pid instead.
+std::string GetSymbolMapDsoName(int pid) {
+  return android::base::StringPrintf("perf-%d.map", pid);
+}
+
+}  // namespace
+
+void ThreadTree::AddSymbolMap(int pid, std::vector<Symbol>* symbols) {
+  auto name = GetSymbolMapDsoName(pid);
+
+  auto dso = FindUserDsoOrNew(name, 0, DSO_SYMBOL_MAP_FILE);
+  dso->SetSymbols(symbols);
+
+  auto thread = FindThreadOrNew(pid, pid);
+  AddThreadMapsForDsoSymbols(thread, dso);
 }
 
 const MapEntry* ThreadTree::AllocateMap(const MapEntry& entry) {

simpleperf/thread_tree_test.cpp
@@ -18,6 +18,8 @@
 
 #include <gtest/gtest.h>
 
+#include "read_symbol_map.h"
+
 using namespace simpleperf;
 
 class ThreadTreeTest : public ::testing::Test {
@@ -62,6 +64,12 @@ class ThreadTreeTest : public ::testing::Test {
         ASSERT_EQ(entry->dso->Path(), expected_names_[i]);
       }
     }
+  }
+
+  const Symbol* FindSymbol(int pid, int tid, uint64_t ip, bool in_kernel = false) {
+    auto thread = thread_tree_.FindThreadOrNew(pid, tid);
+    auto map = thread_tree_.FindMap(thread, ip, in_kernel);
+    return thread_tree_.FindSymbol(map, ip, nullptr, nullptr);
   }
 
   std::vector<std::string> expected_names_;
@@ -119,4 +127,18 @@ TEST_F(ThreadTreeTest, reused_tid_without_thread_exit) {
   // Similar to the above test, but the thread exit record is missing.
   thread_tree_.ForkThread(1, 2, 1, 1);
   thread_tree_.ForkThread(2, 2, 1, 1);
+}
+
+TEST_F(ThreadTreeTest, add_symbol_map) {
+  std::string symbol_map("0x2000 0x20 two\n"
+                         "0x1000 0x10 one\n"
+                         "0x3000 0x30 three\n");
+
+  auto symbols = ReadSymbolMapFromString(symbol_map);
+
+  thread_tree_.AddSymbolMap(1, &symbols);
+
+  ASSERT_STREQ("one", FindSymbol(1, 1, 0x1000)->Name());
+  ASSERT_STREQ("two", FindSymbol(1, 1, 0x2010)->Name());
+  ASSERT_STREQ("three", FindSymbol(1, 1, 0x302f)->Name());
 }

AERA-Recovery/android_system_extras

simpleperf/thread_tree.cpp
@@ -25,6 +25,7 @@
 
 #include "perf_event.h"
 #include "record.h"
+#include "utils.h"
 
 namespace simpleperf {
 
@@ -131,6 +132,35 @@ void ThreadTree::AddThreadMap(int pid, int tid, uint64_t start_addr, uint64_t le
   InsertMap(*thread->maps, MapEntry(start_addr, len, pgoff, dso, false, flags));
 }
 
+void ThreadTree::AddThreadMapsForDsoSymbols(ThreadEntry* thread, Dso* dso) {
+  const uint64_t page_size = GetPageSize();
+
+  auto maps = thread->maps;
+
+  uint64_t map_start = 0;
+  uint64_t map_end = 0;
+
+  // Dso symbols are sorted by address. Walk and calculate containing pages.
+  for (const auto& sym : dso->GetSymbols()) {
+    uint64_t sym_map_start = AlignDown(sym.addr, page_size);
+    uint64_t sym_map_end = Align(sym.addr + sym.len, page_size);
+
+    if (map_end < sym_map_start) {
+      if (map_start < map_end) {
+        InsertMap(*maps, MapEntry(map_start, map_end - map_start, map_start, dso, false, 0));
+      }
+      map_start = sym_map_start;
+    }
+    if (map_end < sym_map_end) {
+      map_end = sym_map_end;
+    }
+  }
+
+  if (map_start < map_end) {
+    InsertMap(*maps, MapEntry(map_start, map_end - map_start, map_start, dso, false, 0));
+  }
+}
+
 Dso* ThreadTree::FindUserDsoOrNew(const std::string& filename, uint64_t start_addr,
                                   DsoType dso_type) {
   auto it = user_dso_tree_.find(filename);
@@ -142,6 +172,27 @@ Dso* ThreadTree::FindUserDsoOrNew(const std::string& filename, uint64_t start_ad
     it = pair.first;
   }
   return it->second.get();
+}
+
+namespace {
+
+// Real map file path depends on where the process can create files.
+// For example, app can create files only in its data directory.
+// Use normalized name inherited from pid instead.
+std::string GetSymbolMapDsoName(int pid) {
+  return android::base::StringPrintf("perf-%d.map", pid);
+}
+
+}  // namespace
+
+void ThreadTree::AddSymbolsForProcess(int pid, std::vector<Symbol>* symbols) {
+  auto name = GetSymbolMapDsoName(pid);
+
+  auto dso = FindUserDsoOrNew(name, 0, DSO_SYMBOL_MAP_FILE);
+  dso->SetSymbols(symbols);
+
+  auto thread = FindThreadOrNew(pid, pid);
+  AddThreadMapsForDsoSymbols(thread, dso);
 }
 
 const MapEntry* ThreadTree::AllocateMap(const MapEntry& entry) {

simpleperf/thread_tree_test.cpp
@@ -18,6 +18,8 @@
 
 #include <gtest/gtest.h>
 
+#include "read_symbol_map.h"
+
 using namespace simpleperf;
 
 class ThreadTreeTest : public ::testing::Test {
@@ -62,6 +64,12 @@ class ThreadTreeTest : public ::testing::Test {
         ASSERT_EQ(entry->dso->Path(), expected_names_[i]);
       }
     }
+  }
+
+  const Symbol* FindSymbol(int pid, int tid, uint64_t ip, bool in_kernel = false) {
+    auto thread = thread_tree_.FindThreadOrNew(pid, tid);
+    auto map = thread_tree_.FindMap(thread, ip, in_kernel);
+    return thread_tree_.FindSymbol(map, ip, nullptr, nullptr);
   }
 
   std::vector<std::string> expected_names_;
@@ -119,4 +127,18 @@ TEST_F(ThreadTreeTest, reused_tid_without_thread_exit) {
   // Similar to the above test, but the thread exit record is missing.
   thread_tree_.ForkThread(1, 2, 1, 1);
   thread_tree_.ForkThread(2, 2, 1, 1);
+}
+
+TEST_F(ThreadTreeTest, add_symbols_for_process) {
+  std::string symbol_map("0x2000 0x20 two\n"
+                         "0x1000 0x10 one\n"
+                         "0x3000 0x30 three\n");
+
+  auto symbols = ReadSymbolMapFromString(symbol_map);
+
+  thread_tree_.AddSymbolsForProcess(1, &symbols);
+
+  ASSERT_STREQ("one", FindSymbol(1, 1, 0x1000)->Name());
+  ASSERT_STREQ("two", FindSymbol(1, 1, 0x2010)->Name());
+  ASSERT_STREQ("three", FindSymbol(1, 1, 0x302f)->Name());
 }
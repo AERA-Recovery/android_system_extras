jbeich/platform_system_extras

Add simpleperf testcase support for riscv64
Signed-off-by: haocheng.zy <haocheng.zy@linux.alibaba.com>
Signed-off-by: Mao Han <han_mao@linux.alibaba.com>
Change-Id: Iadfa43f00237ad62a80e53b8a6581edcf48c7c6f

simpleperf/cmd_record_test.cpp
@@ -192,6 +192,9 @@ TEST(record_cmd, rN_event) {
   } else if (GetTargetArch() == ARCH_X86_32 || GetTargetArch() == ARCH_X86_64) {
     // As in volume 3 chapter 19 of the Intel manual, 0x00c0 is the event number for instruction.
     event_number = 0x00c0;
+  } else if (GetBuildArch() == ARCH_RISCV64) {
+    // RISCV_PMU_INSTRET = 1
+    event_number = 0x1;
   } else {
     GTEST_LOG_(INFO) << "Omit arch " << GetTargetArch();
     return;

simpleperf/cmd_stat_test.cpp
@@ -67,6 +67,9 @@ TEST(stat_cmd, rN_event) {
   } else if (GetTargetArch() == ARCH_X86_32 || GetTargetArch() == ARCH_X86_64) {
     // As in volume 3 chapter 19 of the Intel manual, 0x00c0 is the event number for instruction.
     event_number = 0x00c0;
+  } else if (GetBuildArch() == ARCH_RISCV64) {
+    //  RISCV_PMU_INSTRET = 1
+    event_number = 0x1;
   } else {
     GTEST_LOG_(INFO) << "Omit arch " << GetTargetArch();
     return;
@@ -83,6 +86,8 @@ TEST(stat_cmd, pmu_event) {
     event_string = "cpu/instructions/";
   } else if (GetTargetArch() == ARCH_ARM64) {
     event_string = "armv8_pmuv3/inst_retired/";
+  } else if (GetBuildArch() == ARCH_RISCV64) {
+    event_string = "cpu/instructions/";
   } else {
     GTEST_LOG_(INFO) << "Omit arch " << GetTargetArch();
     return;

simpleperf/test_util.cpp
@@ -43,6 +43,10 @@ bool IsInNativeAbi() {
       if (s.find("arm") == std::string::npos && s.find("aarch64") == std::string::npos) {
         in_native_abi = 0;
       }
+    } else if (GetBuildArch() == ARCH_RISCV64) {
+      if (s.find("riscv") == std::string::npos) {
+        in_native_abi = 0;
+      }
     }
   }
   return in_native_abi == 1;

AERA-Recovery/android_system_extras

simpleperf/cmd_record_test.cpp
@@ -192,6 +192,9 @@ TEST(record_cmd, rN_event) {
   } else if (GetTargetArch() == ARCH_X86_32 || GetTargetArch() == ARCH_X86_64) {
     // As in volume 3 chapter 19 of the Intel manual, 0x00c0 is the event number for instruction.
     event_number = 0x00c0;
+  } else if (GetTargetArch() == ARCH_RISCV64) {
+    // RISCV_PMU_INSTRET = 1
+    event_number = 0x1;
   } else {
     GTEST_LOG_(INFO) << "Omit arch " << GetTargetArch();
     return;

simpleperf/cmd_stat_test.cpp
@@ -67,6 +67,9 @@ TEST(stat_cmd, rN_event) {
   } else if (GetTargetArch() == ARCH_X86_32 || GetTargetArch() == ARCH_X86_64) {
     // As in volume 3 chapter 19 of the Intel manual, 0x00c0 is the event number for instruction.
     event_number = 0x00c0;
+  } else if (GetTargetArch() == ARCH_RISCV64) {
+    //  RISCV_PMU_INSTRET = 1
+    event_number = 0x1;
   } else {
     GTEST_LOG_(INFO) << "Omit arch " << GetTargetArch();
     return;
@@ -83,6 +86,8 @@ TEST(stat_cmd, pmu_event) {
     event_string = "cpu/instructions/";
   } else if (GetTargetArch() == ARCH_ARM64) {
     event_string = "armv8_pmuv3/inst_retired/";
+  } else if (GetTargetArch() == ARCH_RISCV64) {
+    event_string = "cpu/instructions/";
   } else {
     GTEST_LOG_(INFO) << "Omit arch " << GetTargetArch();
     return;

simpleperf/test_util.cpp
@@ -43,6 +43,10 @@ bool IsInNativeAbi() {
       if (s.find("arm") == std::string::npos && s.find("aarch64") == std::string::npos) {
         in_native_abi = 0;
       }
+    } else if (GetTargetArch() == ARCH_RISCV64) {
+      if (s.find("riscv") == std::string::npos) {
+        in_native_abi = 0;
+      }
     }
   }
   return in_native_abi == 1;